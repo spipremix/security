2199d22b30f32471a3e7e437d0035c671ea0412d
Cerdic <cedric@yterium.com>
2013-05-13 21:21:22 +0000

    Report de http://core.spip.org/projects/spip/repository/revisions/20520 et http://core.spip.org/projects/spip/repository/revisions/20521 : mise a jour de _IS_BOT et version

--- ecran_securite.php
+++ ecran_securite.php
@@ -5,7 +5,7 @@
  * ------------------
  */
 
-define('_ECRAN_SECURITE', '1.1.5'); // 8 mars 2013
+define('_ECRAN_SECURITE', '1.1.6'); // 13 mai 2013
 
 /*
  * Documentation : http://www.spip.net/fr_article4200.html
@@ -23,8 +23,12 @@ if (isset($_GET['test_ecran_securite']))
 if (!defined('_IS_BOT'))
 	define('_IS_BOT',
 		isset($_SERVER['HTTP_USER_AGENT'])
-		AND preg_match(',bot|slurp|crawler|spider|webvac|yandex|INA dlweb|EC2LinkFinder|80legs,i',
-			(string) $_SERVER['HTTP_USER_AGENT'])
+		AND preg_match(
+	    // mots generiques
+	    ',bot|slurp|crawler|spider|webvac|yandex|'
+	    // UA plus cibles
+	    . '80legs|accoona|AltaVista|ASPSeek|Baidu|Charlotte|EC2LinkFinder|eStyle|Google|INA dlweb|Java VM|LiteFinder|Lycos|Rambler|Scooter|ScrubbyBloglines|Yahoo|Yeti'
+	    . ',i',(string) $_SERVER['HTTP_USER_AGENT'])
 	);
 
 /*
@@ -305,3 +309,4 @@ if (
 
 
 ?>
+
